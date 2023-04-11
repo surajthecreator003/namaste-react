2a92e7ff7b7c9020d9972ee4e86605b741ad9c1f
Suraj Mallick <surajhustlermallick@gmail.com>
2023-04-11 12:03:25 +0530

    first  push

---
+++ app.js
@@ -0,0 +1,11 @@
+const heading=React.createElement("div",{id:"id1"},[React.createElement("h1",{id:"id2"},"Hello World from React"),React.createElement("h1",{id:"id3"},"Hello World from React")]);
+console.log(heading);
+
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+var a=5;
+
+
+
+
+
