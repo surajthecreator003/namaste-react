92cc85d7e346460b7bf31f2cd5c4bfda79e197bd
Suraj Mallick <surajhustlermallick@gmail.com>
2023-04-11 19:48:47 +0530

    adding react withouth using npx create react app

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
 const heading=React.createElement("div",{id:"id1"},[React.createElement("h1",{id:"id2"},"Hello World from React"),React.createElement("h1",{id:"id3"},"Hello World from React")]);
 console.log(heading);
 
 const root=ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
 var a=5;
-
 
 
 
