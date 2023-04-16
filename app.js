e8c90aa914e33cd386b143be30b3423536366b76
Suraj Mallick <surajhustlermallick@gmail.com>
2023-04-16 11:48:27 +0530

    before starting food ordering app project ep4

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import ReactDOM from "react-dom/client";
 
 // const newLocal = "Namaste React ";
@@ -16,30 +16,30 @@ const root=ReactDOM.createRoot(document.getElementById("root"));
 
 
 
-const Headingjsx=React.createElement("h1",{class:"clr"},"React Element");
-console.log(Headingjsx);
+// const Headingjsx=React.createElement("h1",{class:"clr"},"React Element");
+// console.log(Headingjsx);
 
-const Headingjsx1=()=> <h2 className="clr">React Component</h2>;
-console.log(Headingjsx1);
+// const Headingjsx1=()=> <h2 className="clr">React Component</h2>;
+// console.log(Headingjsx1);
 
-const TrailReactelement=()=> React.createElement("h2",{class:"clr"},"React Element Returned by arrow Function");
-console.log(TrailReactelement);
+// const TrailReactelement=()=> React.createElement("h2",{class:"clr"},"React Element Returned by arrow Function");
+// console.log(TrailReactelement);
 
-function Lol(){
-return React.createElement("h2",{class:"clr"},"React Element Returned by Normal  Function");
-}
-console.log(Lol);
+// function Lol(){
+// return React.createElement("h2",{class:"clr"},"React Element Returned by Normal  Function");
+// }
+// console.log(Lol);
 
-const a1=<h1>helllo</h1>;
+const a1=<h1 class="clr">helllo</h1>;
 
 
-const Jsxinsidereact=()=>React.createElement("div",{class:"clr"},"From insiide arrow function returning rect element will it be a functional component");
-console.log(Jsxinsidereact);//gets printed as js object
+// const Jsxinsidereact=()=>React.createElement("div",{class:"clr"},"From insiide arrow function returning rect element will it be a functional component");
+// console.log(Jsxinsidereact);//gets printed as js object
 
-const val="{using }";
-const elm= <h1>Lets {val} <Headingjsx1/> </h1>;
+// const val="{using }";
+// const elm= <h1>Lets {val} <Headingjsx1/> </h1>;
 
-const A2=()=>(<h1 className="clr">heyyy Non functional jsx element</h1>);
-const r=React.createElement("h1",{},<A2/>);
+// const A2=()=><h1 className="clr">heyyy Non functional jsx element</h1>;
+// const r=React.createElement("h1",{},<A2/>);
 
-root.render(<A2 />);
+root.render(a1);
